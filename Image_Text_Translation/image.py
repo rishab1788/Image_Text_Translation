ba93ccd5496124f5361c733661933f6099488c5f
Rishabh Gupta <rishab178@gmail.com>
2019-10-23 15:31:43 +0530

    Update image.py

--- Image_Text_Translation/image.py
+++ Image_Text_Translation/image.py
@@ -1,18 +1,21 @@
-
-from PIL import Image, ImageDraw
-
+from PIL import Image, ImageDraw, ImageFont
 
 def main():
     try:
         # Relative Path
-        img = Image.open("dp.jpg")
+        g = raw_input("Enter name Of Certifier : ") 
+        fontFile="/home/rishabhgupta/ImageProject/LucidaTypewriterBold.ttf"
+        img = Image.open("/home/rishabhgupta/ImageProject/DSM.PNG")
+        font = ImageFont.truetype(fontFile, 25)
+
         width, height = img.size
         area = (0, 0, width / 2, height / 2)
-
-        d = ImageDraw.Draw(img)
-        d.text((45, 45), "Hello World", fill=(255, 255, 0))
-        # Saved in the same relative location
-        img.save("cropped_picture.jpg")
+        d = ImageDraw.Draw(img) 
+       
+        #d.text((245, 245), "Hello World", fill=(101, 123, 123))
+        fill=(0, 0, 0)
+        d.text((110, 300), g, font=font, fill=fill)
+        img.save("cropped_picture.png")
     except IOError:
         pass
 
